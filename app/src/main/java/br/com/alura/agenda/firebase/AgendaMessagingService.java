dc070da350b873ed8c900deb0917e2c7218c4535
Adilson S. Utiama <adilson.utiama@gmail.com>
2017-06-21 22:37:05 -0300

    Convertendo informacoes recebidas pelo servidor

--- app/src/main/java/br/com/alura/agenda/firebase/AgendaMessagingService.java
+++ app/src/main/java/br/com/alura/agenda/firebase/AgendaMessagingService.java
@@ -2,10 +2,15 @@ package br.com.alura.agenda.firebase;
 
 import android.util.Log;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.IOException;
 import java.util.Map;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.dto.AlunoSync;
 
 /**
  * Created by Adilson on 21/06/2017.
@@ -19,5 +24,23 @@ public class AgendaMessagingService extends FirebaseMessagingService {
 
         Map<String, String> mensagem = remoteMessage.getData();
         Log.i("mensagem recebida", String.valueOf(mensagem));
+
+        converteParaAluno(mensagem);
+    }
+
+    private void converteParaAluno(Map<String, String> mensagem) {
+        if(mensagem.containsKey("alunoSync")){
+            String chaveDeAcesso = "alunoSync";
+            String json = mensagem.get(chaveDeAcesso);
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                AlunoSync alunoSync = mapper.readValue(json, AlunoSync.class);
+                AlunoDAO dao = new AlunoDAO(this);
+                dao.sincroniza(alunoSync.getAlunos());
+                dao.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
